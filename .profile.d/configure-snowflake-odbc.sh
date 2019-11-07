a3fe43323db396e39edfe66f67bb1b3ab5010dc4
Andres Murcia <amurcia@picap.co>
2019-11-07 16:09:06 -0500

    updated snowflake driver

--- .profile.d/configure-snowflake-odbc.sh
+++ .profile.d/configure-snowflake-odbc.sh
@@ -4,15 +4,18 @@ export ODBCSYSINI=${HOME}/.apt/usr/lib/snowflake/odbc/conf/
 
 mkdir -p /etc
 echo "[snowflake]
-Description=SnowflakeDB
-Driver=SnowflakeDSIIDriver
-Locale=en-US
-SERVER=${SNOWFLAKE_ACCOUNT}.snowflakecomputing.com
-database=${SNOWFLAKE_DATABASE}
-uid=${SNOWFLAKE_UID}
-schema=${SNOWFLAKE_SCHEMA}
-warehouse=${SNOWFLAKE_WHAREHOUSE}
-role=${SNOWFLAKE_ROLE}
+[ODBC Data Sources]
+picapodbc1 = Snowflake
+
+[picapodbc1]
+Driver      = SnowflakeDSIIDriver
+Description = Picap ODBC
+uid         = ${SNOWFLAKE_UID}
+server      = ${SNOWFLAKE_ACCOUNT}.snowflakecomputing.com
+database    = ${SNOWFLAKE_DATABASE}
+schema      = ${SNOWFLAKE_SCHEMA}
+warehouse   = ${SNOWFLAKE_WHAREHOUSE}
+role        = ${SNOWFLAKE_ROLE}
 PORT=443
 SSL=on
 CLIENT_SESSION_KEEP_ALIVE=true
