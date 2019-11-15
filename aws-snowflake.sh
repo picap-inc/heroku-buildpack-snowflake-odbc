8ada9b1f3ae6180d44a99b443372e13682d0428e
Andres Murcia <amurcia@picap.co>
2019-11-15 09:41:35 -0500

    aws snowflake

---
+++ aws-snowflake.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export ODBCSYSINI=${HOME}/usr/lib/snowflake/odbc/conf
+
+mkdir -p /etc
+echo "[snowflake]
+[ODBC Data Sources]
+picapodbc1 = Snowflake
+
+[picapodbc1]
+Driver      = SnowflakeDSIIDriver
+Description = Picap ODBC
+uid         = ${SNOWFLAKE_UID}
+server      = ${SNOWFLAKE_ACCOUNT}.snowflakecomputing.com
+database    = ${SNOWFLAKE_DATABASE}
+schema      = ${SNOWFLAKE_SCHEMA}
+warehouse   = ${SNOWFLAKE_WHAREHOUSE}
+role        = ${SNOWFLAKE_ROLE}
+PORT=443
+SSL=on
+CLIENT_SESSION_KEEP_ALIVE=true
+" > ${ODBCSYSINI}/odbc.ini
